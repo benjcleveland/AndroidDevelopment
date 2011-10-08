c832be1c079b7e86960e225f2831950b8a84af83
ben <ben.j.cleveland@gmail.com>
2011-10-08 13:08:07 -0700

    Modified for homework 1.  Added textlayout.xml and TextActivity.java.

--- Homework1/src/edu/washington/cleveb/Homework1Activity.java
+++ Homework1/src/edu/washington/cleveb/Homework1Activity.java
@@ -1,18 +1,36 @@
 package edu.washington.cleveb;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
 
 public class Homework1Activity extends Activity {
-    /** Called when the activity is first created. */
+
+	private Button textButton;
+	
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // set the content view
+        setContentView( R.layout.main );
+
+        // create the text button
+        textButton = (Button) findViewById(R.id.activity_switcher);
+        textButton.setOnClickListener( new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent myIntent = new Intent( Homework1Activity.this, TextActivity.class);
+				Homework1Activity.this.startActivity( myIntent );
+			}
+		});
         
-        TextView tv = new TextView( this );
-        tv.setText( "Hello, Android\n I like turtles!" );
-        
-        setContentView( tv );
-    }
+    }    
 }
+
---
+++ Homework1/src/edu/washington/cleveb/TextActivity.java
@@ -0,0 +1,48 @@
+package edu.washington.cleveb;
+
+import java.util.Random;
+
+import android.app.Activity;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class TextActivity extends Activity {
+    private Button quitButton;
+    private TextView randomTextView;
+    
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    
+        // set the content view
+        setContentView( R.layout.textlayout );
+    
+        // get the string array
+        Resources res = getResources();
+        String[] random_text = res.getStringArray( R.array.random_strings );
+    
+        // get the text view
+        randomTextView = (TextView) findViewById( R.id.random_text );
+    
+        // get a random index
+        Random rand = new Random();
+        
+        // set the text to a random value
+        randomTextView.setText( random_text[rand.nextInt(random_text.length)] );
+        
+        // setup the quit button
+        quitButton = (Button) findViewById( R.id.quit_button );
+        quitButton.setOnClickListener( new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				// put the task in the background so we go back to the home screen
+				moveTaskToBack(true);
+			}
+		});
+    }
+}
