7061260ab725f95a4dd0281002dcd5328d011a7a
ben <ben.j.cleveland@gmail.com>
2011-10-11 18:54:01 -0700

    Changed HomeActivity.java name

---
+++ Homework1/src/edu/washington/cleveb/HomeActivity.java
@@ -0,0 +1,35 @@
+package edu.washington.cleveb;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class HomeActivity extends Activity {
+
+	private Button switchButton;
+	
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // set the content view
+        setContentView( R.layout.main );
+
+        // create the text button
+        switchButton = (Button) findViewById(R.id.activity_switcher);
+        switchButton.setOnClickListener( new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				// start the new activity
+				Intent myIntent = new Intent( HomeActivity.this, TextActivity.class);
+				startActivity( myIntent );
+			}
+		});
+    }    
+}
+
