eb71c58a70917b1ff9d05b8a98accceede07e20c
Ben Cleveland <ben.j.cleveland@gmail.com>
2011-10-16 11:23:13 -0700

    Added initial homework2 project.

---
+++ homework2/src/edu/washington/cleveb/Homework2Activity.java
@@ -0,0 +1,13 @@
+package edu.washington.cleveb;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Homework2Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
