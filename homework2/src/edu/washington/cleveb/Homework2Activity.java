820e1ace2a5aec6ce3dd21feaf366a92f1af3284
Ben Cleveland <ben.j.cleveland@gmail.com>
2011-10-16 12:03:44 -0700

    Basic list view is working with items in it.

--- homework2/src/edu/washington/cleveb/Homework2Activity.java
+++ homework2/src/edu/washington/cleveb/Homework2Activity.java
@@ -1,13 +1,27 @@
 package edu.washington.cleveb;
 
 import android.app.Activity;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class Homework2Activity extends Activity {
-    /** Called when the activity is first created. */
+ 
+	private ListView listview;
+	
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.list);
+        
+        // fill in the values for the list view
+       /* listview = (ListView) findViewById( R.id.itemlistview );
+        
+        Resources res = getResources();
+        
+        listview.setAdapter( new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, res.getStringArray(R.array.text_string_list)));
+        */
     }
 }
