bf7e615e70847db4cdf3b9516b30ea74b399b559
Ben Cleveland <ben.j.cleveland@gmail.com>
2011-11-13 13:13:45 -0800

    Got alot of stuff working.

--- homework4/src/com/washington/cleveb4/Homework4Activity.java
+++ homework4/src/com/washington/cleveb4/Homework4Activity.java
@@ -4,17 +4,97 @@ import android.app.ListActivity;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.CursorAdapter;
 import android.widget.SimpleCursorAdapter;
 import android.widget.Spinner;
 
 public class Homework4Activity extends ListActivity {
 	
+	private static final String PHRASE_TABLE = "phrase";
+	private static final String CATEGORY = "category";
 	private SQLiteDatabase langdb;
 	private CursorAdapter dataSource;
 	private CursorAdapter categorySource;
+	private CursorAdapter languageSource;
 	
 	private Spinner categorySpinner;
+	private Spinner languageSpinner;
+	
+	private OnItemSelectedListener category_listener = new OnItemSelectedListener()
+	{
+
+		public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position,
+				long id) {
+			// get the current cursor
+			Cursor c = categorySource.getCursor();
+			
+			Log.v("hw4", "cursor value" + c.getString(c.getColumnIndex("name")) );
+			
+			String selection = "category = '" + c.getString(c.getColumnIndex("name")) +"'";
+			
+			// we need to update the listview selection
+			Cursor new_cursor = langdb.query(PHRASE_TABLE, new String[] {"english", "spanish", "_ID"}, selection, null, null, null, null);
+			
+			// update the listview cursor
+			dataSource.changeCursor(new_cursor);
+			
+			// notify the listview the data changed
+			dataSource.notifyDataSetChanged();
+		}
+
+		public void onNothingSelected(AdapterView<?> parentView) {
+			// TODO Auto-generated method stub
+			
+		}
+	};
+
+	private OnItemSelectedListener language_listener = new OnItemSelectedListener()
+	{
+
+		public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position,
+				long id) {
+			// get the current cursor
+			Cursor c = languageSource.getCursor();
+			
+			Log.v("hw4", "cursor value" + c.getString(c.getColumnIndex("language")) );
+			
+			// update the language
+			changeLanguage(c.getString(c.getColumnIndex("language")));
+		}
+
+		public void onNothingSelected(AdapterView<?> parentView) {
+			// TODO Auto-generated method stub
+			
+		}
+	};
+	
+	// change the primary language
+	private void changeLanguage(String lang)
+	{
+		int[] order;
+		Log.v("hw4", "language = " + lang );
+		if( lang.equals("English") )
+		{
+			order = new int[]{ android.R.id.text1, android.R.id.text2 };
+		}
+		else
+		{
+			order = new int[] { android.R.id.text2, android.R.id.text1 };
+		}
+		
+		// update the category cursor
+		Cursor categoryCursor = langdb.query("category", new String[] {"_ID", "name" },	"language = '" + lang +"'", null, null, null, null);
+		categorySource.changeCursor(categoryCursor);
+		
+		// change the order of the languages
+		dataSource = new SimpleCursorAdapter( getBaseContext(), android.R.layout.simple_list_item_2, dataSource.getCursor(), new String[] {"english", "spanish" }, order );
+		setListAdapter(dataSource);
+	}
+	
 	
     /** Called when the activity is first created. */
     @Override
@@ -30,16 +110,30 @@ public class Homework4Activity extends ListActivity {
         categorySpinner = (Spinner) findViewById( R.id.category_spinner );
         
         // get the cursor for the category spinner
-        Cursor category_data = langdb.query("language", new String[] {"_id","category"}, null, null, null, null, null);
-        categorySource = new SimpleCursorAdapter( this, android.R.layout.simple_spinner_item, category_data, new String[] {"category"}, new int[]{android.R.id.text1});
+        Cursor category_data = langdb.query("category", new String[]{"_ID", "name"}, "language = 'English'", null, null, null, null);
+        
+        categorySource = new SimpleCursorAdapter( this, android.R.layout.simple_spinner_item, category_data, new String[] {"name"}, new int[]{android.R.id.text1});
         
         // set the adapter for the spinner
         categorySpinner.setAdapter( categorySource );
         
-        // get the cursor for the list view
-        Cursor data = langdb.query("language", null, "category = 'medical questions'", null, null, null, null);
-    
+        // set the click listener
+        categorySpinner.setOnItemSelectedListener( category_listener );
         
+        // setup the language spinner
+        languageSpinner = (Spinner) findViewById( R.id.language_spinner );
+        Cursor language_data = langdb.query("types", new String[] {"_ID", "language" }, null, null, null, null, null);
+        
+        // create the spinner adapter
+        languageSource = new SimpleCursorAdapter( this, android.R.layout.simple_spinner_item, language_data, new String[]{"language"}, new int[]{android.R.id.text1});
+        languageSpinner.setAdapter(languageSource);
+        
+        languageSpinner.setOnItemSelectedListener( language_listener );
+        
+        
+        // get the cursor for the list view
+        Cursor data = langdb.query(PHRASE_TABLE, new String[] {"english", "spanish", "_ID"}, "category = 'medical questions'", null, null, null, null);
+    
         dataSource = new SimpleCursorAdapter( this, android.R.layout.simple_list_item_2, data, new String[] {"english", "spanish" }, new int[]{android.R.id.text1, android.R.id.text2 } );
         
         // connect the database to the list
--- homework4/src/com/washington/cleveb4/LanguageDatabase.java
+++ homework4/src/com/washington/cleveb4/LanguageDatabase.java
@@ -4,13 +4,15 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.provider.BaseColumns;
+import android.util.Log;
 
 public class LanguageDatabase extends SQLiteOpenHelper {
 
-	private static final int DATABASE_VERSION = 1;
+	private static final int DATABASE_VERSION = 8;
 	private static final String DATABASE_NAME = "LanguageDatabase";
-	private static final String DATABASE_CREATE = "CREATE TABLE language (" + BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, category VARCHAR, spanish VARCHAR, english VARCHAR)";
-	
+	private static final String DATABASE_CREATE = "CREATE TABLE phrase (" + BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, category VARCHAR, spanish VARCHAR, english VARCHAR, FOREIGN KEY ('category') REFERNCES category('_ID')";
+	private static final String TABLE2_CREATE = "CREATE TABLE types (" + BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, language VARCHAR)";
+	private static final String TABLE3_CREATE = "CREATE TABLE category (" + BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR, language VARCHAR, FOREIGN KEY ('language') REFERENCES phrase (" + BaseColumns._ID + "))";
 	
 	public LanguageDatabase(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -21,16 +23,35 @@ public class LanguageDatabase extends SQLiteOpenHelper {
 	public void onCreate(SQLiteDatabase db) {
 		// "IF NOT EXISTS"
 		db.execSQL(DATABASE_CREATE);
-
+		db.execSQL(TABLE2_CREATE);
+		db.execSQL(TABLE3_CREATE);
+		
 		// fill in some initial data
 		// this is only for testing
-		db.execSQL("insert into language (category, spanish, english) values ('medical questions', 'spanish words', 'english words')" );
-		db.execSQL("insert into language (category, spanish, english) values ('Greeting questions', 'spanish words1', 'english words2')" );
+		db.execSQL("insert into phrase (category, spanish, english) values ('medical questions', 'spanish words', 'english words')" );
+		db.execSQL("insert into phrase (category, spanish, english) values ('Greeting questions', 'spanish words1á', 'english words2')" );
+		db.execSQL("insert into phrase (category, spanish, english) values ('Greeting questions', 'spanish words3é', 'english words3')" );
+		db.execSQL("insert into phrase (category, spanish, english) values ('Greeting questions', 'spanish words4úó', 'english words4')" );
+		
+		// update the types table
+		db.execSQL("insert into types (language) values ('English')" );
+		db.execSQL("insert into types (language) values ('Español')" );
+		
+		db.execSQL("insert into category (language, name) values ('English', 'Greeting Questions')");
+		db.execSQL("insert into category (language, name) values ('Español', 'Preguntas Saludo')");
+		db.execSQL("insert into category (language, name) values ('English', 'Medical Questions')");
+		db.execSQL("insert into category (language, name) values ('Español', 'Algunas de las preguntas')");
 	}
 
 	@Override
-	public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
-		// TODO Auto-generated method stub
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		Log.w(LanguageDatabase.class.getName(), "Upgrading database from " + oldVersion + " to " + newVersion );
+		// clear out the database
+		db.execSQL("DROP TABLE IF EXISTS phrase" );
+		db.execSQL("DROP TABLE IF EXISTS types");
+		db.execSQL("DROP TABLE IF EXISTS category");
+		// create a new one
+		onCreate(db);
 	}
 
 }
