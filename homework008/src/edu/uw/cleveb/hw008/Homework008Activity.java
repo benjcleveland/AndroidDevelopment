a7ea784d7355e3e47b4696fd00b16e32d3e255c6
Ben Cleveland <ben.j.cleveland@gmail.com>
2012-01-07 20:30:42 -0800

    Adding initial files for homework 008.

---
+++ homework008/src/edu/uw/cleveb/hw008/Homework008Activity.java
@@ -0,0 +1,13 @@
+package edu.uw.cleveb.hw008;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Homework008Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
