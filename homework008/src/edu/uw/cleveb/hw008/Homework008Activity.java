85e88c3b5b1b7453252bfa8561540cd9e4950f21
Ben Cleveland <ben.j.cleveland@gmail.com>
2012-01-08 14:45:11 -0800

    Forecast is now working.  The user can also change the zip code.

--- homework008/src/edu/uw/cleveb/hw008/Homework008Activity.java
+++ homework008/src/edu/uw/cleveb/hw008/Homework008Activity.java
@@ -16,14 +16,27 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnKeyListener;
+import android.view.inputmethod.InputMethodManager;
+import android.webkit.WebView;
+import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 public class Homework008Activity extends Activity {
 	
 	private static TextView current_condition;
 	private static TextView current_temp;
+	private static WebView forcast;
+	private static EditText zipcode;
+	private static TextView title;
 	
     /** Called when the activity is first created. */
     @Override
@@ -33,21 +46,81 @@ public class Homework008Activity extends Activity {
         
         current_condition = (TextView)findViewById(R.id.current_condition);
         current_temp = (TextView)findViewById(R.id.current_temp);
-        
-        Log.e("here", "this is a test");
-        // get the weather in json format
-        String weather = readWeather( "http://query.yahooapis.com/v1/public/yql?format=json&q=select%20*%20from%20rss%20where%20url=%27http://xml.weather.yahoo.com/forecastrss/98057_f.xml%27");
+        forcast = (WebView) findViewById(R.id.webview);
+        zipcode = (EditText) findViewById(R.id.zip_code_text);
+        title = (TextView) findViewById(R.id.title);
+       
+        zipcode.setOnKeyListener(new OnKeyListener(){
+			public boolean onKey(View v, int keyCode, KeyEvent event) {
+				if( keyCode == KeyEvent.KEYCODE_ENTER){
+					// update the weather display
+					displayWeather( zipcode.getText().toString());
+					// hide the softkeyboard
+					InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+					imm.hideSoftInputFromWindow(zipcode.getWindowToken(), 0);
+					return true;
+				}
+				return false;
+			}
+        });
+        // fill in the weather for the default zip code
+        displayWeather("98057");
+    }
+    
+    // updates the screen to display the weather from the given zipcode
+    private void displayWeather( String zipcode )
+    {
+    	// get the weather in json format
+    	String query = String.format("http://query.yahooapis.com/v1/public/yql?format=json&q=select%%20*%%20from%%20rss%%20where%%20url=%%27http://xml.weather.yahoo.com/forecastrss/%s_f.xml%%27", zipcode);
+        String weather = readWeather( query ); 
         Log.e("here", weather);
         
         try
         {
         	JSONObject jsonObject = new JSONObject( weather );
         	
-        	JSONObject condition = jsonObject.getJSONObject("query").getJSONObject("results").getJSONObject("item").getJSONObject("condition");
+        	JSONObject item = jsonObject.getJSONObject("query").getJSONObject("results").getJSONObject("item");
+        	JSONObject condition = item.getJSONObject("condition");
+        	JSONArray forcast_array = item.getJSONArray("forecast");
         	Log.i("here", condition.getString("temp"));
         	
         	current_condition.setText(condition.getString("text"));
         	current_temp.setText(condition.getString("temp"));
+        	title.setText( item.getString("title"));
+        	
+        	forcast.loadData(item.getString("description"), "text/html", null);
+        	
+        	LayoutInflater inflater = getLayoutInflater();
+        	TableLayout forecast_table = (TableLayout) findViewById(R.id.forcast_table);
+        	// fill in the forcast table
+        	for( int i = 0; i < forcast_array.length(); ++i )
+        	{
+        		// get the json object out of the array
+        		JSONObject day_forecast = forcast_array.getJSONObject(i);
+        		
+        		TableRow forecast_row = (TableRow) forecast_table.getChildAt(i);
+        		if( forecast_row == null)
+        		{
+        			// inflate a new row layout
+        			forecast_row = (TableRow) inflater.inflate(R.layout.forecast_row, null);
+        				
+        			// add the table row to the table
+        			forecast_table.addView(forecast_row);
+        		}
+        			
+        		// get all the resources that we need
+        		TextView day = (TextView) forecast_row.findViewById(R.id.forecast_day);
+        		TextView text = (TextView) forecast_row.findViewById(R.id.forecast_text);
+        		TextView high = (TextView) forecast_row.findViewById(R.id.forecast_high);
+        		TextView low = (TextView) forecast_row.findViewById(R.id.forecast_low);
+        		
+        		// fill in the data from the json object
+        		day.setText(day_forecast.getString("day"));
+        		text.setText(day_forecast.getString("text"));
+        		high.setText(day_forecast.getString("high"));
+        		low.setText(day_forecast.getString("low"));
+        	}
+        	
         } catch( Exception e )
         {
         	e.printStackTrace();
