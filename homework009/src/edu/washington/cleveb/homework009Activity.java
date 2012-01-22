c67caa4432e5f721308af27770521ec0d1ff1ceb
Ben Cleveland <ben.j.cleveland@gmail.com>
2012-01-22 11:29:51 -0800

    Initial commit of HW 009

---
+++ homework009/src/edu/washington/cleveb/homework009Activity.java
@@ -0,0 +1,14 @@
+package edu.washington.cleveb;
+
+import edu.washington.cleveb.hw009.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class homework009Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
